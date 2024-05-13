954c7470f9a8ead83eed6b259ecdbf5fc8959a96
mobarak10 <mobarak.hossain.cse12@gmail.com>
2024-05-13 09:20:19 +0600

    update user model & create transaction model migration

---
+++ database/migrations/2024_05_13_031445_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->enum('transaction_type', ['deposit', 'withdraw'])->default('deposit');
+            $table->double('amount')->default(0);
+            $table->decimal('fee', 10, 2)->default(0.00);
+            $table->date('date')->comment('transaction date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
